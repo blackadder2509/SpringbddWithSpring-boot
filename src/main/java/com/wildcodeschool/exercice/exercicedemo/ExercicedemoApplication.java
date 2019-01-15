564bc2e7ddf67cc5839be89cd81573909237ecbc
Anabelle Baynard <baynardanabelle@gmail.com>
2019-01-15 15:18:02 +0100

    first commit

---
+++ src/main/java/com/wildcodeschool/exercice/exercicedemo/ExercicedemoApplication.java
@@ -0,0 +1,14 @@
+package com.wildcodeschool.exercice.exercicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicedemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/wildcodeschool/exercice/exercicedemo/ExercicedemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wildcodeschool.exercice.exercicedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExercicedemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
